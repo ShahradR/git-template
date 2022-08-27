9fc996a588b462c06573b06521a616e77f7b35ec
Shahrad Rezaei <shahrad@rezaei.io>
2022-08-27 21:05:33 +0000

    feat: add codespell to the project
    Add codespell to the project, a tool to "check code for common
    misspellings".
    
    A configuration file is also added to ignore files in the vale/
    directory since they are sourced from an external repository and
    codespell generates false positives on some of the Vale configuration
    files.
    
    This tool is installed in the development container and as a pre-commit
    hook. See https://bit.ly/3AuIOKo for more details.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,8 @@
 pre-commit install --install-hooks
 pre-commit install --install-hooks --hook-type commit-msg
 
+pip install codespell
+
 npm install -g commitizen
 npm install -g cz-conventional-changelog
 npm install -g markdown-link-check
