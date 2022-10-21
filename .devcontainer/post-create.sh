7da63179967c17a4af15618e5a9435dfbc437b0d
Shahrad Rezaei <shahrad@rezaei.io>
2022-10-21 08:31:52 +0000

    docs: use the Material for MkDocs theme
    Use the Material for MkDocs theme for the project's documentation. This
    commit also installs the theme to the development container.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,7 +3,7 @@
 pre-commit install --install-hooks
 pre-commit install --install-hooks --hook-type commit-msg
 
-pip install codespell mkdocs
+pip install codespell mkdocs mkdocs-material
 
 npm install -g commitizen
 npm install -g cz-conventional-changelog
